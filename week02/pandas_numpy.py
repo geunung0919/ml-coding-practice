c7b1c402d0f0cde6f97f233433c869485ee47d59
김근형 <nhunk94@gmail.com>
2026-03-18 12:19:15 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -35,4 +35,6 @@ print(sample_df.loc['a':'c'])
 
 #iloc: integer location의 약자
 print(sample_df.iloc[[0,1,2]])
-print(sample_df)
+print(sample_df.iloc[0:2])
+print(sample_df.iloc[0:3])
+print(sample_df.iloc[0:3, 2:4])     #컬럼까지 동시에 인덱싱
