90bac23e7fe08b2b94b96bfa24211132accfa8d1
김근형 <nhunk94@gmail.com>
2026-03-18 13:05:15 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -203,3 +203,19 @@ print(np.random.randin(50,71, 10))
 print(np.random.choice(['red', 'green', 'white', 'black','blue'], size = 3))
 print(np.random.choice(['red','green','white','black','blue'],size = 3, replace = False))
 
+print(np.arange(1,11))
+print(np.arange(1,11,2))
+print(np.linspace(1,100,10))        #1부터 10까지 균등한 간격의 값을 4개 추출
+
+A = np.array([4, 16, 25])
+print(np.sqrt(A))
+
+print(np.arange(8).reshape(2,4) + 10)
+
+#0부터 8미만까지 출력하고 (2,4) 크기로 재가공 후 , 제곱하여 출력
+a = np.arange(8).reshape(2,4) ** 2
+print(a)
+
+print(a.sum())      #모든 요소의 합 
+print(a.mean())     #모든 요소의 평균
+print()
