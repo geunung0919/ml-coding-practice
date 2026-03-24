3ff0a947ffc475a903c29bd725a7fcc0766e44fb
김근형 <nhunk94@gmail.com>
2026-03-25 02:05:45 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -155,4 +155,16 @@ plt.title('Correlation Heatmp of Titanic')
 plt.savefig('Figure07.png')
 plt.close()
 
-## **영역 채우기 그래프 
+## **영역 채우기 그래프 : 나이대별 생존자와 사망자 수 표현하기**
+
+#결측치 처리
+titanic = titanic.dropna(subset=['Age','Fare'])
+
+#나이대별 생존자와 사망자 수 계산하기 위해 범주형 변수로 변환
+age_groups = pd.cut(titanic['Age'], bins= range(0,81,5))
+
+#Age, Survived 기준으로 그룹화 
+survived_counts = titanic.groupby([age_groups,'Survived'], observed = False).size().unstack().fillna(0)
+print(survived_counts)
+
+#영역 채우기 그래프 그리기
