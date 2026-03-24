4665fc76c4691ba4ad55ad53832e353a35b3bd6d
김근형 <nhunk94@gmail.com>
2026-03-25 02:22:15 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -220,4 +220,16 @@ violin_plot = plt.violinplot([titanic[titanic['Pclass'] == 1]['Age'],
                               titanic[titanic['Pclass'] == 3]['Age']],
                               showmeans=False, showmedians=True)
 
-plt.title('')
+plt.title('Violin Plot of Age by Pclass')
+plt.xlabel('Pclass')
+plt.ylabel('Age')
+
+# x축의 눈금 설정
+plt.xticks([1,2,3], ['1st Class','2nd Class', '3rd Class'])
+
+# 범례 설정
+plt.legned(violin_plot['bodies'], ['1st Class', '2nd Class', '3rd Class'],
+           title = 'Pclass', loc = "upper right")
+plt.savefig('Figure10.png')
+plt.close()
+
