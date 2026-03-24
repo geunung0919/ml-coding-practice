a630e2d8a4ec51f4c4bced78940ca8c5cb70b09f
김근형 <nhunk94@gmail.com>
2026-03-25 02:31:46 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -273,3 +273,11 @@ plt.subplot(2,2,4)
 plt.plot([10,11,12])
 plt.savefig('Figure12.png')
 plt.close()
+
+## **타이타닉 데이터셋으로 개별 서브플롯 하나씩 그리기**
+
+#Survived가 - 이면 사망자를 ,1이면 생존자를 나타냄
+titanic = pd.read_csv('3.1.1.titanic.csv')
+
+#각 부모와 자녀의 수에 따른 생존자와 사망자 수 계산
+parch_counts = 
