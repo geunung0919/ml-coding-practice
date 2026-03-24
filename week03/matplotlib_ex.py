12e0a8ad7f2cff833edb74fcd9666246b1757d6c
김근형 <nhunk94@gmail.com>
2026-03-25 02:02:44 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -129,4 +129,30 @@ plt.hist(titanic['Age'], bins = 20, color = 'seagreen', edgecolor = 'black')
 
 plt.xlabel('Age')
 plt.ylabel('Count')
-plt
+plt.title('Distribution of ages on the Titanic')
+plt.grid(axis = 'y', linestyle = '--', alpha = 0.7)
+plt.savefig('Figure06.png')
+plt.close()
+
+## 히트맵 : 두 변수의 상관 관계를 표시하기**
+
+# 결측치 처리
+titanic = titanic.dropna(subset=['Age','Fare'])
+
+# 항관 행렬 계산
+correlation_matrix = titanic.drop('PassengerId', axis = 1).corr(numeric_only=True)
+print(correlation_matrix)
+
+# 히트맵 그리기
+plt.matshow(correlation_matrix, cmap='PuRd_r')
+plt.colorbar()
+
+# x축과 y축의 눈금 설정 
+plt.xticks(range(len(correlation_matrix.columns)), correlation_matrix.columns, rotation = 45)
+plt.yticks(range(len(correlation_matrix.columns)), correlation_matrix.columns)
+
+plt.title('Correlation Heatmp of Titanic')
+plt.savefig('Figure07.png')
+plt.close()
+
+## **영역 채우기 그래프 
