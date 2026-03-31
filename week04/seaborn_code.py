348b840ba59e07fde1cf2fb42771359e3fd5591e
김근형 <nhunk94@gmail.com>
2026-03-31 13:25:33 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -73,6 +73,9 @@ plt.savefig('./week04/Seaborn_Figure04.jpg')
 
 # **조인트 그래프**
 # pairplot( ) 그리기
+
+# **관계 그래프**
+# pairplot( ) 그리기
 sns.pairplot(data = tips, hue ='sex', diag_kind = 'hist', palette='husl')
 
 plt.suptitle('Pairplot with Histogram by Gender', y= 1.05)
