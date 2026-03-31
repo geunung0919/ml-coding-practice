01261ec4fa6dd1a75f690b639c7f1daf506e7158
김근형 <nhunk94@gmail.com>
2026-03-31 13:42:33 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -28,3 +28,15 @@ housing["income_cat"] = pd.cut(housing["median_income"],
 
 strat_train_set, strat_test_set = train_test_split(
     housing, test_size = 0.2, stratify=housing["income_cat"], random_state = 42)
+
+for set_ in (strat_train_set, strat_test_set):
+    set_.drop("income_cat", axis = 1, inplace=True)
+
+"""
+*원본 훈련 세트로 복원하고 타깃을 분리
+*'strat_train_set.drop()'은 지정한 열을 제외한 'strat_train_set'의 복사본을 만듦
+*'inplace=True'로 지정하지 않은 한 'strat_train_set' 자체를 수정하지 않음
+"""
+
+housing = strat_train_set.drop("median_house_value", axis = 1)
+housing_labels = strat_train_set[]
