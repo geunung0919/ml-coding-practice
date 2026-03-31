f42cad8929eb5bb35091c23d39bc9bbd762b8bce
김근형 <nhunk94@gmail.com>
2026-03-31 13:34:33 +0900

    Updated Changes

---
+++ week04/data_preprocessing.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# 데이터 준비
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import tarfile
+import urllib.request
+
+def load_housing_data():
+    tarball_path = Path("datasets/housing.tgz")
+    if not tarball_path.is_file():
+        Path("datasets").mkdir(parents=True, exist_ok=True)
+        url = 
