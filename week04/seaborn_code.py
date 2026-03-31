8d0950679afed8cb743bc2c568254b99d3be2991
김근형 <nhunk94@gmail.com>
2026-03-31 13:26:33 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -73,6 +73,8 @@ plt.savefig('./week04/Seaborn_Figure04.jpg')
 
 # **조인트 그래프**
 # pairplot( ) 그리기
+sns.jointplot(x ='size', y='tip', data= tips, kind='scatter')
+plt.savefig('./week04/Seaborn_Figure05.jpg')
 
 # **관계 그래프**
 # pairplot( ) 그리기
