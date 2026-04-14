883679d90602510e2ef3fdaab5edf0a40ad9bbdc
김근형 <nhunk94@gmail.com>
2026-04-14 13:16:24 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -54,4 +54,9 @@ def getNaverSearch(node, srcText, page_start, display):
 def getRequestUrl(url):
     req = urllib.request.Request(url)
     
-    
+    req.add_header("X-Naver-Client-Id", client_id)
+    req.add_header("X-Naver-Client-Secret", client_secret)
+
+    try:
+        response = urllib.request.urlopen(req)
+        if re
