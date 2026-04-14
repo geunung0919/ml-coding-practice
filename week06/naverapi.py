210e43630e4d07b51bbb0623f330cf02cddcdf5c
김근형 <nhunk94@gmail.com>
2026-04-14 13:14:57 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -51,3 +51,7 @@ def getNaverSearch(node, srcText, page_start, display):
         return json.loads(responseDecode)
     
 
+def getRequestUrl(url):
+    req = urllib.request.Request(url)
+    
+    
