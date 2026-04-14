a0d58330c5a5a34a1daaca21024b5be34a63f956
김근형 <nhunk94@gmail.com>
2026-04-14 13:01:26 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -1,1 +1,13 @@
-# -*-coding: uft
+# -*-coding: uft-8 -*-
+import urllib.request
+import datetime
+import json
+
+client_id = 'Client ID'
+client_secret = 'Client Secret'
+
+def main():
+
+    node = 'news'                                       #크롤링할 대상
+    srcText = input('검색어를 입력하세요:   ')
+    
