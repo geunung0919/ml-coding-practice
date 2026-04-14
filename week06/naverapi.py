6d9826e1837840f1d52a01af7af413c13d9af64e
김근형 <nhunk94@gmail.com>
2026-04-14 13:20:24 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -68,4 +68,13 @@ def getRequestUrl(url):
         return None 
     
 
-def getPostData(post, jsonResult, cnt)   #[CODE 3]
+def getPostData(post, jsonResult, cnt):   #[CODE 3]
+    title = post['title']
+    description = post['description']
+    org_link = post['originallink']
+    link = post['link']
+
+    #
+    #
+    #
+    #
