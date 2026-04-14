37a7bea0085ccc56d501fea620a4dac0c3c0bcfc
김근형 <nhunk94@gmail.com>
2026-04-14 13:05:54 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -25,4 +25,6 @@ def main():
             start = jsonRespones['start'] + jsonRespones['display']
             jsonRespones = getNaverSearch(node, srcText, start, 100)  # [CODE 2]
 
+            print('전체 검색 : %d 건 ' %total)
+
             
