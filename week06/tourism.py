957c6b7f42b361189b6b5bd9d963be1343e29f71
김근형 <nhunk94@gmail.com>
2026-04-14 14:31:52 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -60,4 +60,5 @@ def main():
 def getTourismStatsItem(yyyymm, nat_cd, ed_cd):
     service_url = "http://openapi.tour.go.kr/openapi/service/EdrcntTourismStatsService/getEdrcntTourismStatsList"
     parameters = "?_type=json&serviceKey=" + ServiceKey     #인증키
-    ㅔ
+    parameters += "&YM=" + yyyymm
+    parameters += "&NAT_CD=" +
