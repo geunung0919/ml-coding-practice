1990acd4cb252af16aeb369375583abe00d8d7dd
김근형 <nhunk94@gmail.com>
2026-04-14 13:25:54 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -80,4 +80,13 @@ def getPostData(post, jsonResult, cnt):   #[CODE 3]
     pDate = datetime.datetime.strptime(post['pubDate'], '%a, %d %b %Y %H:%M:%S +0900')
     pDate = pDate.strftime('%Y-%m-%d %H:%M:%S')
 
-    jsonResult.append({})
+    jsonResult.append({
+        'cnt': cnt,
+        'title':title,
+        'description': description,
+        'org_link': org_link,
+        'link': link,
+        'pDate':pDate
+    })
+    return
+
