f8ac6a5352da14e1149602547cfaa35ae3cd6aff
김근형 <nhunk94@gmail.com>
2026-04-14 13:02:57 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -14,4 +14,5 @@ def main():
     cnt = 0
     jsonResult = []
 
-    jsonRespones = getNaverSearch(node, srcText, 1, 100)        #[]
+    jsonRespones = getNaverSearch(node, srcText, 1, 100)        #[CODE 2]
+    total = jsonResponse['total']
