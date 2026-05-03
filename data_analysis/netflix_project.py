72c8729fdeb20d48ed15c81537c4aaf5fd684c19
김근형 <nhunk94@gmail.com>
2026-05-03 22:54:57 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -181,7 +181,7 @@ netflix_age_country_unstack
 plt.figure(figsize=(15,5))
 
 # 사용자 정의 컬러맵 만들기
-cmap = plt.matplotlib.color.LinearsSegmentedColormap.from_list('', ['#221f1f', '#b20710', '#f5f5f1'])
+cmap = plt.matplotlib.color.LinearSegmentedColormap.from_list('', ['#221f1f', '#b20710', '#f5f5f1'])
 
 sns.heatmap(netflix_age_country_unstack, cmap = cmap, linewidth=2.5, annot=True, fmt='.0%')
 
