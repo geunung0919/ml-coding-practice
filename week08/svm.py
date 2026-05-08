369d300d770dc278dfd35c59a68bdb7f65759ee1
김근형 <nhunk94@gmail.com>
2026-05-08 21:49:33 +0900

    Updated Changes

--- week08/svm.py
+++ week08/svm.py
@@ -27,3 +27,17 @@ from sklearn.preprocessing import PolynomialFeatures
 
 X, y = make_moons(n_samples=100, noise=0.15, random_state=42)
 
+polynomial_svm_clf = make_pipeline(
+    PolynomialFeatures(degree=3),
+    StandardScaler(),
+    LinearSVC(C=10, max_iter=10_000, dual=True, random_state=42)
+)
+polynomial_svm_clf.fit(X,y)
+
+# 다항식 커널
+
+from sklearn.svm import SVC
+
+poly_kernel_svm_clf = make_pipeline(StandardScaler(),
+                                    SVC(kernel="poly", degree=3, coef0=1, C=5))
+poly_kernel_svm_clf.fit(X,y)
