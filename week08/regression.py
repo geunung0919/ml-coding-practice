8e30f3a704dd7e7dce1161cff4cec7849d18c980
김근형 <nhunk94@gmail.com>
2026-05-08 21:30:33 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 plt.rc('font', size=14)
 plt.rc('axes', labelsize=14, titlesize=14)
 plt.rc('legend', fontsize=14)
-plt.rc('xtick', lavelsize=10)
+plt.rc('xtick', labelsize=10)
 plt.rc('ytick', labelsize=10)
 
 import numpy as np
