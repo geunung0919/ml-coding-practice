c78b4c165ae2b1883716073a84bf009b75d999b0
김근형 <nhunk94@gmail.com>
2026-05-08 21:20:03 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -131,5 +131,6 @@ from sklearn.linear_model import ElasticNet
 
 elastic_net = ElasticNet(alpha=0.1, l1_ratio=0.5)
 elastic_net.fit(X,y)
-print(lasso_reg.predict([[1.5]]))
+print(elastic_net.predict([[1.5]]))
 
+ 
