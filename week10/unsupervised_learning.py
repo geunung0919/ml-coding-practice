c0dd6425612f939428cdd480d5d5ac7200622114
김근형 <nhunk94@gmail.com>
2026-05-31 17:13:19 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -29,3 +29,11 @@ def plot_clusters(X, y=None):
     plt.scatter(X[:, 0], X[:, 1], c=y, s=1)
     plt.xlabel("$x_1$")
     plt.ylabel("$x_2$", rotation=0)
+
+plt.figure(figsize=(8,4))
+plot_clusters(X)
+plt.gca().set_axisbelow(True)
+plt.grid()
+plt.show()
+
+# 각 샘플은 5개의 클러스터 중 하나에 할당 
