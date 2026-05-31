f752177dd940f2fc3daf7380fe92489c88e0ffc5
김근형 <nhunk94@gmail.com>
2026-05-31 17:55:49 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -152,3 +152,7 @@ def plot_dbscan(dbscan, X, size, show_xlabels=True, show_ylabels=True):
     else:
         plt.tick_params(labelleft = False)
     plt.title(f"eps={dbscan.eps:.2f}, min_samples={dbscan.min_samples}")
+    plt.grid()
+    plt.gca().set_axisbelow(True)
+
+dbscan2 
