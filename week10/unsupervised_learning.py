afab3c56e97df9875c10a2bad8aff8e9c311d3d8
김근형 <nhunk94@gmail.com>
2026-05-31 17:28:49 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -64,4 +64,6 @@ plt.show()
 
 from sklearn.metrics import silhouette_score
 
-print(silhouette_score)
+print(silhouette_score(X, kmeans.labels_))
+
+silhouette_score = [silhouette_score(X, model.labels)
