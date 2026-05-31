974e37bea17dd59c463ec94e4ba29a9ff8349cde
김근형 <nhunk94@gmail.com>
2026-05-31 17:34:49 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -80,3 +80,15 @@ from sklearn.metrics import silhouette_samples
 from matplotlib.ticker import FixedLocator, FixedFormatter
 
 plt.figure(figsize=(11, 9))
+
+for k in (3,4,5,6):
+    plt.subplot(2,2,k-2)
+
+    y_pred - kmeans_per_k[k-1].labels_
+    silhouette_coefficients = silhouette_samples(X, y_pred)
+
+    padding = len(X) // 30
+    pos = padding
+    ticks = []
+    for i in range(k):
+        
